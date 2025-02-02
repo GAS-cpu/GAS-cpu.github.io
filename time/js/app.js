b416f10a5e3de75f4ff512263d37d9972daca0f0
GAS-cpu <141532672+GAS-cpu@users.noreply.github.com>
2025-02-02 16:08:58 +0800

    Update app.js

--- time/js/app.js
+++ time/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     $(".date").before(times);
 
     //春节倒计时
-    var starttime = new Date("2025/1/28");
+    var starttime = new Date("2026/2/17");
     setInterval(function () {
         var nowtime = new Date();
         var time = starttime - nowtime;
